8c55c4cd6d164b1ded0044bb307062e8a84cd8d2
Herman Jackson <hermanjackson561@gmail.com>
2021-08-13 13:28:37 -0400

    updated py

--- med_flask/app.py
+++ med_flask/app.py
@@ -37,10 +37,10 @@ def sendSMS():
 def hello():
     return "hello"
 
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
-
 
 
 
